7618fa2be64fd4997b4c21548e4cde8505f5e3a0
Aruniya <121487536+AruniyaAsokan@users.noreply.github.com>
2025-04-19 08:24:01 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -338,21 +338,4 @@ function updateRoute(id, latitude, longitude) {
   ]);
 }
 
-// changess
-function getPhoneLocation() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-
-      console.log("Lat:", lat, "Lon:", lon);
-
-      fetch(`http://192.168.4.1/location?lat=${lat}&lon=${lon}`);
-    });
-  } else {
-    alert("Geolocation not supported");
-  }
-}
-
-setInterval(getPhoneLocation, 10000);
 
